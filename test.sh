06e38eb566f9c440bce81a0c5c1e16bb318640ca
Xinhui Li <sucyli1017@gmail.com>
2020-05-10 12:40:49 -0400

    update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,12 @@
 # local
-python muSkullStrip.py -in /Users/xinhui.li/Documents/monkey-segmentation/data/human_init_test/test/t1w/959574_3T_T1w_MPR1.nii.gz -out /Users/xinhui.li/Documents/monkey-segmentation/data/out/test -model /Users/xinhui.li/Documents/monkey-segmentation/data/out/test/model-00-epoch
+# python muSkullStrip.py -in /Users/xinhui.li/Documents/monkey-segmentation/data/human_init_test/test/t1w/959574_3T_T1w_MPR1.nii.gz -out /Users/xinhui.li/Documents/monkey-segmentation/data/out/test -model /Users/xinhui.li/Documents/monkey-segmentation/data/out/test/model-00-epoch
 # python muSkullStrip.py -in /Users/xinhui.li/Documents/research/data/monkey/test/head_in_brain.nii.gz -out /Users/xinhui.li/Documents/research/data/out/aws/monkey-with-human-model/wm -model /Users/xinhui.li/Documents/research/data/out/aws/hcp/wm/model-09-epoch
 # python muSkullStrip.py -in /Users/xinhui.li/Documents/research/data/monkey/test/head_in_brain.nii.gz -out /Users/xinhui.li/Documents/research/data/out/aws/monkey-with-human-model/gm -model /Users/xinhui.li/Documents/research/data/out/aws/hcp/gm/model-09-epoch
 # python muSkullStrip.py -in /Users/xinhui.li/Documents/research/data/monkey/test/head_in_brain.nii.gz -out /Users/xinhui.li/Documents/research/data/out/aws/monkey-with-human-model/csf -model /Users/xinhui.li/Documents/research/data/out/aws/hcp/csf/model-09-epoch
 # python muSkullStrip.py -in /Users/xinhui.li/Documents/monkey-segmentation/data/hcp/test/t1w -out /Users/xinhui.li/Documents/monkey-segmentation/data/out/aws/hcp/tissue -model /Users/xinhui.li/Documents/monkey-segmentation/data/out/aws/hcp/tissue/model-09-epoch -class 7
+
+# monkey
+python muSkullStrip.py -in /Users/xinhui.li/Documents/monkey-segmentation/data/monkey/site-mountsinai-P/preprocessed_T1 -out /Users/xinhui.li/Documents/monkey-segmentation/data/monkey/site-mountsinai-P_unet -model /Users/xinhui.li/Documents/monkey-segmentation/data/out/aws/hcp/fix_bg/monkey/model-09-epoch
 
 # aws 
 # python muSkullStrip.py -in /media/ebs/data/hcp/test/t1w -out /media/ebs/out/hcp/wm -model /media/ebs/out/hcp/wm/model-09-epoch # function to save dice fails 
