0d3b26a07422a492c75615743ddb51681be36737
Xinhui Li <sucyli1017@gmail.com>
2020-08-17 13:36:23 -0400

    update plot

--- test.sh
+++ test.sh
@@ -12,8 +12,6 @@
 # python muSkullStrip.py -in /Users/xinhui.li/Documents/monkey-segmentation/data/monkey/site-mountsinai-P/site-mountsinai-P_preprocessed_T1 -out /Users/xinhui.li/Documents/monkey-segmentation/data/monkey/site-mountsinai-P_unet -model /Users/xinhui.li/Documents/monkey-segmentation/data/out/aws/hcp/fix_bg/monkey/model-09-epoch
 python muSkullStrip.py -in /Users/xinhui.li/Documents/monkey-segmentation/data/monkey/site-mountsinai-P/site-mountsinai-P_preprocessed_T1 -out /Users/xinhui.li/Documents/monkey-segmentation/data/monkey/site-mountsinai-P_unet2 -model /Users/xinhui.li/Documents/monkey-segmentation/data/monkey/stef/mountsinai-P/model-09-epoch
 
-
-
 # aws 
 # python muSkullStrip.py -in /media/ebs/data/hcp/test/t1w -out /media/ebs/out/hcp/wm -model /media/ebs/out/hcp/wm/model-09-epoch # function to save dice fails 
 # python muSkullStrip.py -in /media/ebs/data/hcp/test/t1w -out /media/ebs/out/hcp/gm -model /media/ebs/out/hcp/gm/model-09-epoch
